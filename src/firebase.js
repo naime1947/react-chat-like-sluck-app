038892ebfc15ce79ba39ec95cedaa5b1db74af02
Naime Ahmed <naime1947@gmail.com>
2020-04-23 16:17:14 +0600

    firebase authentication file created

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/storage'
+
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCU3u9kF2f76nBybQx5Vv8YpnC5rA2J2BQ",
+    authDomain: "react-slack-chat-app-f6915.firebaseapp.com",
+    databaseURL: "https://react-slack-chat-app-f6915.firebaseio.com",
+    projectId: "react-slack-chat-app-f6915",
+    storageBucket: "react-slack-chat-app-f6915.appspot.com",
+    messagingSenderId: "534901209532",
+    appId: "1:534901209532:web:22bbb04039a6fb31f893b9",
+    measurementId: "G-3HRMQEPD2R"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  export default firebase;
