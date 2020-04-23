0fe02f45fb80a6d4bfe2973d9ae421367b9aaf64
Naime Ahmed <naime1947@gmail.com>
2020-04-23 14:41:11 +0600

    folder organized and login register component created and routing added

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Login from './Auth/Login'
+import Register from './Auth/Register'
+
+function App() {
+  return (
+    <div className="App">
+      hello from App
+      <Login/>
+      <Register/>
+    </div>
+  );
+}
+
+export default App;
