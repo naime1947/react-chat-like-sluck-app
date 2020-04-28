a849021daaa704812b599667a6e6b3963c5796a4
Naime Ahmed <naime1947@gmail.com>
2020-04-29 01:14:07 +0600

    registraion of user completed

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -11,6 +11,7 @@ import {
 } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import firebase from "../../firebase";
+import md5 from "md5";
 
 class Register extends Component {
   state = {
@@ -18,7 +19,9 @@ class Register extends Component {
     email: "",
     password: "",
     passwordConfirmation: "",
-    errors:[]
+    errors: [],
+    loading: false,
+    usersRef: firebase.database().ref("users"),
   };
   handleChange = (event) => {
     this.setState({
@@ -28,71 +31,112 @@ class Register extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
     if (this.isFormValid()) {
-      this.setState({loading:true, errors:[]});
+      this.setState({ loading: true, errors: [] });
       firebase
         .auth()
         .createUserWithEmailAndPassword(this.state.email, this.state.password)
         .then((createdUser) => {
-          console.log(createdUser);
-          this.setState({loading:false});
-
-          this.setState({
-            username: "",
-            email: "",
-            password: "",
-            passwordConfirmation: "",
-          });
-
+          createdUser
+          .user
+            .updateProfile({
+              displayName: this.state.username,
+              photoURL: `https://www.gravatar.com/avatar/${md5(
+                this.state.email
+              )}?d=identicon`,
+            })
+            .then(() => {
+              this.saveUser(createdUser).then(() => {
+                this.setState({
+                  username: "",
+                  email: "",
+                  password: "",
+                  passwordConfirmation: "",
+                  loading: false,
+                  errors:[]
+                });
+              });
+            })
+            .catch((error) => {
+              console.log(error);
+              this.setState({
+                errors: this.state.errors.concat(error),
+                loading: false,
+              });
+            });
         })
         .catch((error) => {
           console.error(error);
-          this.setState({loading:false, errors:this.state.errors.concat(error)});
+          this.setState({
+            loading: false,
+            errors: this.state.errors.concat(error),
+          });
         });
-
-      
     }
   };
-  isFormValid = ()=>{
+
+  saveUser = (createdUser) => {
+    return this.state.usersRef.child(createdUser.user.uid).set({
+      name: createdUser.user.displayName,
+      avatar: createdUser.user.photoURL,
+    });
+  };
+
+  isFormValid = () => {
     let errors = [];
     let error;
-    if(this.isFieldsEmpty(this.state)){
-      error = {message:"Fill in all fields"};
-      this.setState({errors:errors.concat(error)});
+    if (this.isFieldsEmpty(this.state)) {
+      error = { message: "Fill in all fields" };
+      this.setState({ errors: errors.concat(error) });
       return false;
-    }
-    else if(!this.isPasswordValid(this.state)){
-      error = {message:"Password is invalid"};
-      this.setState({errors:errors.concat(error)});
-      return false;
-    }
-    return true;
-  }
-
-  isFieldsEmpty = ({username, email, password, passwordConfirmation})=>{
-    return (!username.length || !email.length || !password.length || !passwordConfirmation.length);
-  }
-  isPasswordValid = ({password, passwordConfirmation})=>{
-    if(password.length<6 || passwordConfirmation.length<6){
-      return false;
-    }else if(passwordConfirmation!==password){
+    } else if (!this.isPasswordValid(this.state)) {
+      error = { message: "Password is invalid" };
+      this.setState({ errors: errors.concat(error) });
       return false;
     }
     return true;
-  }
+  };
 
-  //displayError = errors => errors.map((error, i)=> <p key={i}>{error.message}</p> ) 
+  isFieldsEmpty = ({ username, email, password, passwordConfirmation }) => {
+    return (
+      !username.length ||
+      !email.length ||
+      !password.length ||
+      !passwordConfirmation.length
+    );
+  };
+  isPasswordValid = ({ password, passwordConfirmation }) => {
+    if (password.length < 6 || passwordConfirmation.length < 6) {
+      return false;
+    } else if (passwordConfirmation !== password) {
+      return false;
+    }
+    return true;
+  };
+
+  //displayError = errors => errors.map((error, i)=> <p key={i}>{error.message}</p> )
   displayError = (errors) => {
-    return ( errors.map((error,i)=>{
-    return <p key={i}>{error.message}</p>
-    }) );
-  }
+    return errors.map((error, i) => {
+      return <p key={i}>{error.message}</p>;
+    });
+  };
 
-  handleInputError = (errors, inputName)=>{
-    return errors.some(error=>error.message.includes(inputName))?"error":"";
-  }
+  handleInputError = (errors, inputName) => {
+    return errors.some((error) =>
+      error.message.toLowerCase().includes(inputName)
+    )
+      ? "error"
+      : "";
+  };
 
   render() {
-    const { username, email, password, passwordConfirmation, errors,loading } = this.state;
+    const {
+      username,
+      email,
+      password,
+      passwordConfirmation,
+      errors,
+      loading,
+    } = this.state;
     return (
       <Grid textAlign="center" verticalAlign="middle" className="app">
         <Grid.Column style={{ maxWidth: 450 }}>
@@ -146,11 +190,17 @@ class Register extends Component {
                 className={this.handleInputError(errors, "password")}
               />
             </Segment>
-            <Button disabled={loading} className={loading?"loading":""} size="large" color="orange" fluid>
+            <Button
+              disabled={loading}
+              className={loading ? "loading" : ""}
+              size="large"
+              color="orange"
+              fluid
+            >
               Submit
             </Button>
           </Form>
-          {errors.length>0 && (
+          {errors.length > 0 && (
             <Message error>
               <h3>Error !</h3>
               {this.displayError(errors)}
