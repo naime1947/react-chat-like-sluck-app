03679d6c57ff5761455ea0ddcb25e064e3f15beb
Naime Ahmed <naime1947@gmail.com>
2020-05-07 01:48:02 +0600

    Redirecting to app compoent after login successfull

--- src/index.js
+++ src/index.js
@@ -1,29 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-import Login from './components/Auth/Login';
-import Register from './components/Auth/Register';
-import 'semantic-ui-css/semantic.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import "semantic-ui-css/semantic.min.css";
+import firebase from "./firebase";
 
+import { BrowserRouter as Router, Switch, Route, withRouter } from "react-router-dom";
 
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+class Root extends React.Component {
 
+  componentDidMount(){
+    firebase.auth().onAuthStateChanged(user => {
+        this.props.history.push("/");
+    });
+  }
 
-const Root = ()=>(
-  <Router>
-    <Switch>
-      <Route exact path="/" component={App}/>
-      <Route path="/login" component={Login}/>
-      <Route path="/register" component={Register}/>
-    </Switch>
-  </Router>
-)
+  render() {
+    return (
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch>
+    );
+  }
+}
+
+const RouteWithAuth = withRouter(Root);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Root/>
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <RouteWithAuth/>
+  </Router>,
+  document.getElementById("root")
 );
 serviceWorker.unregister();
+
+export default Root;
+
